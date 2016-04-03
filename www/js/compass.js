ad699b0574c723f9e772e93032159eb0b5667b7a
RobinVanBallegooij <rga.vanballegooij@student.avans.nl>
2016-04-03 19:31:16 +0200

    added device orientation plugin + compass watch

--- www/js/compass.js
+++ www/js/compass.js
@@ -3,12 +3,12 @@ document.addEventListener("deviceready", setupCompass, false);
 function setupCompass() {
 	console.log("SETUP");
 
-	var loc1 = { latitude: 51.6888639, longitude: 5.2855437 };
-	var loc2 = { latitude: 51.6889038, longitude: 5.2865522 };
-	var loc3 = { latitude: 51.6890235, longitude: 5.2835052 };
-
-	var bearing1 = calculateBearing(loc1.latitude, loc1.longitude, loc2.latitude, loc2.longitude);
-	var bearing2 = calculateBearing(loc1.latitude, loc1.longitude, loc3.latitude, loc3.longitude);
+	//var loc1 = { latitude: 51.6888639, longitude: 5.2855437 };
+	//var loc2 = { latitude: 51.6889038, longitude: 5.2865522 };
+	//var loc3 = { latitude: 51.6890235, longitude: 5.2835052 };
+//
+	//var bearing1 = calculateBearing(loc1.latitude, loc1.longitude, loc2.latitude, loc2.longitude);
+	//var bearing2 = calculateBearing(loc1.latitude, loc1.longitude, loc3.latitude, loc3.longitude);
 
 	//console.log(bearing1);
 	//console.log(bearing2);
@@ -16,9 +16,13 @@ function setupCompass() {
 	//$("#compass").rotate(45);
 
 
-	//actual location test:
+	//geolocation
 	navigator.geolocation.getCurrentPosition(updateLocation);
 	var watchId = navigator.geolocation.watchPosition(updateLocation);
+
+	//compass
+	navigator.compass.getCurrentHeading(updateCompass);
+	navigator.compass.watchHeading(updateCompass);
 
 	$("#button_update").on("click", function() {
 		console.log("update");
@@ -45,6 +49,17 @@ function updateLocation(position) {
 	$("#current_latitude").html("Latitude: " + currentLatitude);
 	$("#current_longitude").html("Longitude: " + currentLongitude);
 	$("#bearing").html("Bearing: " + bearing);
+}
+
+function updateCompass(heading) {
+	var magneticHeading = heading.magneticHeading;
+	console.log(heading);
+	console.log(magneticHeading);
+}
+
+function compassError(error) {
+	console.log("compass error");
+	console.log(error.code);
 }
 
 function calculateBearing(lat1,lng1,lat2,lng2) {
